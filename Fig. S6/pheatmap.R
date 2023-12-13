55423ae122197fb20058a98ed1eb643c2b384de0
Jiulong-Zhao <62932502+Jiulong-Zhao@users.noreply.github.com>
2023-12-13 15:26:54 +0800

    Add files via upload

---
+++ Fig. S6/pheatmap.R
@@ -0,0 +1,19 @@
+library(pheatmap)
+library(ggplot2)
+
+data <- read.table("K-KSW-sharing_vOTUs_vMAGs.txt",header=T,row.names=1,sep="\t",comment.char="")
+data <- log10(data+1)
+
+p <- pheatmap(data, cluster_cols = T, cluster_rows = T,  
+         border_color = "gray",                             
+         angle_col = 90,   
+         color = colorRampPalette(c("#FFFFFF", "#F4E3A1", "#F09F49", "#D84531", "#7C192A"))(50),  
+         cellwidth = 15, cellheight = 15) 
+p
+ggsave(paste("./", "heatmap.pdf", sep=""), p, width = 10, height = 10)
+
+
+
+
+
+
