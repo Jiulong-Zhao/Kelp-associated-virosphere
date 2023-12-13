11c344f567ec9fe216729aa13ea3500a223a80e8
Jiulong-Zhao <62932502+Jiulong-Zhao@users.noreply.github.com>
2023-12-13 15:00:57 +0800

    Add files via upload

---
+++ Fig. 4/a/shannon.boxplot.r
@@ -0,0 +1,151 @@
+#!/usr/bin/env Rscript
+
+
+
+data = "text_upload_file/ad4d7061-9e72-49a4-9ea5-129a75ff7411.txt"
+melted = TRUE
+xvariable = "Group"
+yvariable = "Shannon"
+legend_variable = "NULL"
+xtics_angle = 0
+legend_variable_order = "NULL"
+legend_variable_cut = "NULL"
+xvariable_order = "S3S9_0_3,Others"
+xvariable_cut = "NULL"
+y_add = 0
+ylim = "NULL"
+yaxis_scale_mode = "NULL"
+notch = FALSE
+outlier = FALSE
+out_scale = 1.05
+legend.position = "right"
+manual_color_vector = "#FCE724,#440153"
+violin = FALSE
+violin_nb = FALSE
+scale_violin = "width"
+ID_var = ""
+jitter = FALSE
+jitter_bp = TRUE
+dotplot = FALSE
+dotplot_bp = FALSE
+group_variable_for_line = "NULL"
+group_variable_order_for_line = "NULL"
+coordinate_flip = FALSE
+facet_variable = "NULL"
+facet_variable_order = "NULL"
+x_label = "NULL"
+y_label = "NULL"
+title = "NULL"
+facet_nrow = NULL
+facet_ncol = NULL
+facet_scales = "fixed"
+facet_singlecell_style = FALSE
+gene = "NULL"
+metadata = "NULL"
+filename = "NULL"
+statistics = TRUE
+statistical_threshold_for_letters = 0.05
+width = 8
+height = 9
+debug = FALSE
+outputprefix = "user/_visitors/result_output/boxplot2023-01-08_21:02:39/7615bc3e-3d23-40ae-b95d-40ca61070950"
+outputpictype = "pdf"
+
+if (data == "") {
+  script = sub(".*=", "", commandArgs()[4])
+  #print(script)
+  system(paste(script, "-h"))
+  stop("At least -f is required!")
+}
+
+
+# For all users
+# devtools::install_github("Tong-Chen/ImageGP")
+# For chinese users
+# devtools::install_git("https://gitee.com/ct5869/ImageGP.git")
+library(ImageGP)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(ggbeeswarm)
+library(multcompView)
+library(dplyr)
+library(htmlwidgets)
+library(plotly)
+if (outputprefix == "") {
+  outputprefix = data
+}
+
+filename = paste0(outputprefix,  ".boxplot.", outputpictype)
+
+legend_variable_order = sp_string2vector(legend_variable_order)
+group_variable_order_for_line = sp_string2vector(group_variable_order_for_line)
+legend_variable_cut = sp_string2vector(legend_variable_cut)
+xvariable_order = sp_string2vector(xvariable_order)
+xvariable_cut = sp_string2vector(xvariable_cut)
+ID_var= sp_string2vector(ID_var)
+facet_variable_order = sp_string2vector(facet_variable_order)
+manual_color_vector = sp_string2vector(manual_color_vector)
+if (!sp.is.null(ylim)){
+  ylim = as.numeric(sp_string2vector(ylim))
+}
+if (grepl(",",legend.position)) {
+	  legend.position = as.numeric(sp_string2vector(legend.position))
+}
+
+saveppt = FALSE
+savehtml = FALSE
+cat(sp_current_time(), "Starting...\n")
+
+sp_boxplot(
+  data = data,
+  melted = melted,
+  xvariable = xvariable,
+  yvariable = yvariable,
+  legend_variable = legend_variable,
+  statistics = statistics,
+  statistical_threshold_for_letters = statistical_threshold_for_letters,
+  xtics_angle = xtics_angle,
+  legend_variable_order = legend_variable_order,
+  legend_variable_cut = legend_variable_cut,
+  xvariable_order = xvariable_order,
+  xvariable_cut = xvariable_cut,
+  y_add = y_add,
+  yaxis_scale_mode = yaxis_scale_mode,
+  notch = notch,
+  par = par,
+  outlier = outlier,
+  out_scale = out_scale,
+  legend.position = legend.position,
+  manual_color_vector = manual_color_vector,
+  violin = violin,
+  violin_nb = violin_nb,
+  scale_violin = scale_violin,
+  ID_var = ID_var,
+  jitter = jitter,
+  jitter_bp = jitter_bp,
+  dotplot = dotplot,
+  dotplot_bp = dotplot_bp,
+  group_variable_for_line = group_variable_for_line,
+  group_variable_order_for_line = group_variable_order_for_line,
+  colormodel = colormodel,
+  coordinate_flip = coordinate_flip,
+  facet_variable = facet_variable,
+  facet_variable_order = facet_variable_order,
+  facet_singlecell_style = facet_singlecell_style,
+  x_label = x_label,
+  y_label = y_label,
+  title = title,
+  ylim = ylim,
+  facet_nrow = facet_nrow,
+  facet_ncol = facet_ncol,
+  facet_scales = facet_scales,
+  metadata = metadata,
+  filename = filename,
+  width = width,
+  height = height,
+  saveppt = saveppt,
+  savehtml = savehtml
+)
+cat(sp_current_time(), "Success.\n")
+
---
+++ Fig. 4/b/PCoA.R
@@ -0,0 +1,57 @@
+library(vegan)
+library(ape)
+data <- read.table("KSD-vOTUs_abundance.txt",header=T,row.names=1,sep="\t")
+data <- t(data)
+data[is.na(data)] <- 0
+distance <- vegdist(data, method = "bray")
+pcoa<- pcoa(distance, correction = "none", rn = NULL)
+
+groups <- read.table("metadata.txt",sep = "\t",header = T,colClasses = c("character"))
+groups <- as.list(groups)
+PC1 = pcoa$vectors[,1]
+PC2 = pcoa$vectors[,2]
+plotdata <- data.frame(rownames(pcoa$vectors),PC1,PC2,groups$part)
+colnames(plotdata) <-c("sample","PCo1","PCo2","group")
+
+
+pc1 <-floor(pcoa$values$Relative_eig[1]*100)
+pc2 <-floor(pcoa$values$Relative_eig[2]*100)
+
+library(ggplot2)
+p <- ggplot(plotdata, aes(PCo1, PCo2)) +
+  geom_point(aes(colour=groups$part,fill=groups$part),size=6)+
+  scale_colour_manual(values=c("#440153","#FCE724"))+
+  scale_fill_manual(values=c("#440153","#FCE724"))+
+  labs(title="PCoA") + 
+  xlab(paste("PC1 ( ",pc1,"%"," )",sep="")) + 
+  ylab(paste("PC2 ( ",pc2,"%"," )",sep=""))+
+  theme(text=element_text(size=30))+
+  geom_vline(aes(xintercept = 0),linetype="dotted")+
+  geom_hline(aes(yintercept = 0),linetype="dotted")+
+  xlim(-0.3, 0.8) +
+  ylim(-0.4, 0.4) +
+  theme(panel.background = element_rect(fill='white', colour='black'),
+        panel.grid=element_blank(), 
+        axis.title = element_text(color='black',size=10),
+        axis.ticks.length = unit(0.4,"lines"), axis.ticks = element_line(color='black'),
+        axis.line = element_line(colour = "black"), 
+        axis.title.x=element_text(colour='black', size=10),
+        axis.title.y=element_text(colour='black', size=10),
+        axis.text=element_text(colour='black',size=10),
+        legend.title=element_blank(),
+        legend.text=element_text(size=10),
+        legend.key=element_blank(),
+        legend.background = element_rect(colour = "black"),
+        legend.key.height=unit(0,"cm"))+
+  theme(plot.title = element_text(size=15,colour = "black",hjust = 0.5,face = "bold")) +
+  geom_text(aes(label=sample),size=5,hjust=0.5,vjust=-1)
+
+p
+
+ggsave(paste("./", "PCoA.pdf", sep=""), p, width = 7.5, height = 6)
+
+#PERMANOVA
+comparision.part <- adonis2(data ~ part, data = groups, permutations = 9999,distance = "bray")
+summary(comparision.part)
+capture.output(comparision.part,file = "comparision_part.txt" )
+
