0e68d5804dc2acd4d459ac849307685695d9beae
Jiulong <62932502+Jiulong-Zhao@users.noreply.github.com>
2024-03-22 11:24:14 +0800

    Add files via upload

---
+++ Fig. S3/c/圆角柱状堆叠图.R
@@ -0,0 +1,10 @@
+# install.packages("ggchicklet",repos = "https://cinc.rud.is")                 # Install & load ggchicklet package
+# 参考：https://statisticsglobe.com/draw-ggplot2-barplot-round-corners-r
+                 
+library("ggchicklet")
+library("ggplot2")
+
+df <- read.table("input.txt", header = T)
+
+ggplot(df, aes(group, value, fill = sub)) +    # Stacked barplot with round corners
+  geom_chicklet(radius = grid::unit(3, "mm"))
